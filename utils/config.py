561def34a5156c68dba84ad0fbe24be48c05c736
Vaibhav Pandey <vpandey@clearsense.com>
2023-01-20 12:35:09 -0800

    changed config generation

--- utils/config.py
+++ utils/config.py
@@ -1,18 +1,11 @@
 from configparser import ConfigParser
 import os
 
-if os.environ.get("ENVIRONMENT", "local") in ["local", "staging"]:
-    config_object = ConfigParser()
 
-    config_object.read("config.ini")
-
-    SCHEMA_API = config_object['SCHEMA_API']
-    DB_CONFIG = config_object['CREDENTIALS_DATABASE']
-else:
-    DB_CONFIG = {
-        "USERNAME": os.environ.get("DB_USERNAME"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "NAME": os.environ.get("DB_DATABASE"),
-        "TABLENAME": os.environ.get("DB_TABLENAME")
-    }
+DB_CONFIG = {
+    "USERNAME": os.environ.get("DB_USERNAME"),
+    "PASSWORD": os.environ.get("DB_PASSWORD"),
+    "HOST": os.environ.get("DB_HOST"),
+    "NAME": os.environ.get("DB_DATABASE"),
+    "TABLENAME": os.environ.get("DB_TABLENAME")
+}
